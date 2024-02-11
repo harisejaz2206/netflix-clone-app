8b5d68104ff16c4672d3fda35f21bd34b0db87aa
harisejaz2206 <harisejaz2206@gmail.com>
2024-02-12 02:12:09 +0500

    fixed bug again

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,15 +1,14 @@
-import React from 'react'
-import { Navigate } from 'react-router-dom'
-import { UserAuth } from '../context/AuthContext'
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import { UserAuth } from '../context/AuthContext';
 
-
-const ProtectedRoute = () => {
-    const {user} = UserAuth()
-    if(!user) {
-        return <Navigate to={'/'}/>
+const ProtectedRoute = ({ children }) => {
+    const { user } = UserAuth();
+    if (!user) {
+        return <Navigate to={'/'} />;
     } else {
         return children;
     }
-}
+};
 
-export default ProtectedRoute
+export default ProtectedRoute;
